d1d30f0d2a5a1eb6391f1ea9e463d7d7e9e1f093
Romeo <supanat0245@gmail.com>
2023-02-21 22:21:57 +0700

    add modal choose template

---
+++ client/components/dashboard/card/card.component.tsx
@@ -0,0 +1,8 @@
+const CardComponent = (props: any) => {
+  return (
+    <div className="">
+      <div>asdasd</div>
+    </div>
+  );
+};
+export default CardComponent;
--- client/components/dashboard/modal/createPomodoro.tsx
+++ client/components/dashboard/modal/createPomodoro.tsx
@@ -3,10 +3,12 @@ interface Props {
   setOpenModalCreate: (value: boolean) => void;
 }
 
-const CreatePomodoros = (props:Props) => {
+const CreatePomodoros = (props: Props) => {
   const [pomodoroName, setPomodoroName] = useState<string>("");
 
   const [tagList, setTagList] = useState<string[]>([]);
+
+  const [continueToTemplate, setContinueToTemplate] = useState<boolean>(false);
 
   const addTag = (e: any) => {
     e.preventDefault();
@@ -21,10 +23,88 @@ const CreatePomodoros = (props:Props) => {
     //get input value
   };
 
-  const removeTag = (index:Number) => {
+  const removeTag = (index: Number) => {
     //get input value
     const newTagList = tagList.filter((tag, i) => i !== index);
     setTagList(newTagList);
+  };
+
+  const btnContinue = () => {
+    setContinueToTemplate(true);
+  };
+
+  if (continueToTemplate) {
+    return (
+      <div
+      className="fixed z-30 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        {/* black screen */}
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        ></div>
+        {/* modal space on top */}
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        <div
+          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-3xl sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-title"
+        >
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start w-full">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
+                <h3
+                  className="text-3xl font-medium text-gray-900"
+                  id="modal-title"
+                >
+                 Choose a template 
+                </h3>
+                <div className="mt-2 w-full">asdasdas</div>
+                {/* loop tag list */}
+                <div className="mt-2 w-full flex flex-wrap">
+                  {tagList.map((tag, index) => (
+                    <div
+                      key={index}
+                      className="flex items-center justify-between text-xs px-3 mb-2 bg-purple-500 text-white py-1 rounded-md mr-2 cursor-pointer"
+                      onClick={() => removeTag(index)}
+                    >
+                      <p>{tag}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-600 text-base font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={btnContinue}
+            >
+              Continue
+            </button>
+            <button
+              type="button"
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={(e) => props.setOpenModalCreate(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
   }
 
   return (
@@ -118,13 +198,14 @@ const CreatePomodoros = (props:Props) => {
             <button
               type="button"
               className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-600 text-base font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={btnContinue}
             >
               Continue
             </button>
             <button
               type="button"
               className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={(e)=>props.setOpenModalCreate(false)}
+              onClick={(e) => props.setOpenModalCreate(false)}
             >
               Cancel
             </button>
