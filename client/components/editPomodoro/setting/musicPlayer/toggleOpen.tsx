1e60484e8e5190db14e86a7c247602c28584a698
Romeo <supanat0245@gmail.com>
2023-04-04 20:28:01 +0700

    update

--- client/components/editPomodoro/setting/musicPlayer/toggleOpen.tsx
+++ client/components/editPomodoro/setting/musicPlayer/toggleOpen.tsx
@@ -4,7 +4,7 @@ const EditMusicPlayerToggleOpen = () => {
   return (
     <div
       id="musicPlayerSetting"
-      className="absolute left-14 top-0 bg-black bg-opacity-90 rounded-md px-5 py-2 text-white w-96"
+      className="absolute z-50 text-white left-[63px] hover:bg-opacity-100 bg-opacity-90 bg-black w-[450px] h-[70vh] px-5 py-4 top-1/2 -translate-y-1/2 rounded-md overflow-hidden"
     >
       <FaqSelection>hello1234</FaqSelection>
     </div>
